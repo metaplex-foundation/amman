8c1e7bd4018fb86c864e4538554e6dfff11fc743
Thorsten Lorenz <thlorenz@gmx.de>
2021-12-10 10:09:02 -0700

    actions: exposing via class based API

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,1 +1,10 @@
-export * from './create-mint-account'
+import { Connection, PublicKey } from '@solana/web3.js'
+import { createMintAccount } from './create-mint-account'
+
+export class Actions {
+  constructor(private readonly connection: Connection) {}
+
+  createMintAccount = (payer: PublicKey) => {
+    return createMintAccount(this.connection, payer)
+  }
+}
