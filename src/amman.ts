2adb9e0cfe2c60cd0284ae6beb307edb128feb89
Thorsten Lorenz <thlorenz@gmx.de>
2022-02-22 18:54:25 -0600

    actions: removing mint action since it doesn't belong in amman

--- src/amman.ts
+++ src/amman.ts
@@ -1,7 +1,6 @@
 export * from './validator'
 
 export { airdrop, tmpLedgerDir, LOCALHOST } from './utils'
-export * from './actions'
 export * from './asserts'
 export * from './diagnostics'
 export * from './transactions'
