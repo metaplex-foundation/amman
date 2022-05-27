8a8617a82d177946ab5e87aae3e27b0c29274af0
Thorsten Lorenz <thlorenz@gmx.de>
2022-05-27 18:23:26 -0500

    chore: use diff.Change type

--- amman-client/src/relay/types.ts
+++ amman-client/src/relay/types.ts
@@ -1,7 +1,5 @@
-import { Diff } from 'deep-diff'
-
-// TODO(thlorenz): Used to be Diff.Change
-type DiffChange = any
+import type { Diff } from 'deep-diff'
+import type { Change } from 'diff'
 
 export type AccountDiff = Array<Diff<Record<string, any>, Record<string, any>>>
 export type RelayAccountState = {
@@ -9,6 +7,6 @@ export type RelayAccountState = {
   accountDiff?: AccountDiff
   slot: number
   rendered?: string
-  renderedDiff?: DiffChange[]
+  renderedDiff?: Change[]
   timestamp: number
 }
