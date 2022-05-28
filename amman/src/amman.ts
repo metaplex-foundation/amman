235955ebbb1bf50ebac0c580866a114a255984fa
Thorsten Lorenz <thlorenz@gmx.de>
2022-05-28 12:50:51 -0500

    chore: forwarding Amman export in deprecated mode

--- amman/src/amman.ts
+++ amman/src/amman.ts
@@ -1,9 +1,19 @@
+import * as AmmanClient from '@metaplex-foundation/amman-client'
+
 export { tmpLedgerDir } from './utils'
 export { Change } from './accounts/state'
 export * from './storage/mock-driver'
 export * from './types'
 
+// -----------------
+// Forwarding some amman-client exports
+// -----------------
 export {
   AmmanAccountRendererMap,
   LOCALHOST,
 } from '@metaplex-foundation/amman-client'
+
+/**
+ * @deprecated Use from _amman-client_ directly via `import { Amman } from '@metaplex-foundation/amman-client'`
+ */
+export const Amman = AmmanClient.Amman
