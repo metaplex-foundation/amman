83c6aa436e35e2e2c309b8f4909c24802511287c
Thorsten Lorenz <thlorenz@gmx.de>
2022-05-29 18:11:43 -0500

    validator: harden validator up check

--- amman/src/validator/ensure-validator-up.ts
+++ amman/src/validator/ensure-validator-up.ts
@@ -33,6 +33,7 @@ export async function ensureValidatorIsUp(
     validateStatus: (status: number) => status === 405,
     log: false,
   })
+  await ensureWeb3Connection(connectionURL)
   if (verifyFees) {
     logDebug('Ensuring validator charges fees ...')
 
@@ -40,6 +41,16 @@ export async function ensureValidatorIsUp(
     const connection = new Connection(connectionURL, 'confirmed')
     await airdrop(connection, payer.publicKey, 200)
     return ensureFees(connectionURL, payer)
+  }
+}
+
+async function ensureWeb3Connection(connectionURL: string): Promise<void> {
+  const connection = new Connection(connectionURL, 'confirmed')
+  try {
+    await connection.getAccountInfo(SystemProgram.programId)
+  } catch (err) {
+    await sleep(200)
+    return ensureWeb3Connection(connectionURL)
   }
 }
 
@@ -55,12 +66,12 @@ async function ensureFees(
     toPubkey: receiver.publicKey,
   })
   const transaction = new Transaction().add(transferIx)
-  const recentBlockhash = (await connection.getRecentBlockhash('confirmed'))
+  const recentBlockhash = (await connection.getLatestBlockhash('confirmed'))
     .blockhash
   transaction.recentBlockhash = recentBlockhash
   const sig = await connection.sendTransaction(transaction, [payer])
   await connection.confirmTransaction(sig)
-  const confirmedTx = await connection.getConfirmedTransaction(sig)
+  const confirmedTx = await connection.getTransaction(sig)
 
   if (confirmedTx?.meta?.fee === 0) {
     logDebug('Transaction completed without charging fees, trying again ...')
