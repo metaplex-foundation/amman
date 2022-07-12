7003c27268f20f8dd67b795ac9b2f085b4aeea5b
Thorsten Lorenz <thlorenz@gmx.de>
2022-07-12 16:10:16 -0600

    test: setup amman version test

--- amman-tests/tests/socket-communication.ts
+++ amman-tests/tests/socket-communication.ts
@@ -1,6 +1,18 @@
+import { AMMAN_VERSION } from '@metaplex-foundation/amman/src/relay/types'
+import spok from 'spok'
 import test from 'tape'
+import { killAmman, launchAmman, relayClient } from './utils/launch'
 
-test('socket-communication: amman version', (t) => {
-  t.pass('test coming up')
+const DEBUG = true
+
+test('socket-client: amman version', async (t) => {
+  const client = relayClient()
+
+  const state = await launchAmman({ streamTransactionLogs: DEBUG })
+  {
+    const version = await client.fetchAmmanVersion()
+    spok(t, version, { $topic: 'amman version', ...AMMAN_VERSION })
+  }
+  await killAmman(t, state)
   t.end()
 })
